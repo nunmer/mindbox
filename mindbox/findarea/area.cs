000844dc8fb48bbaf1422cf329770a1e259be4e7
sqnzhqr <45449130+nunmer@users.noreply.github.com>
2022-06-29 03:05:06 +0600

    Add files via upload

---
+++ mindbox/findarea/area.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace findarea
+{
+    public class Area
+    {
+        public static void FindCircleArea(int radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine(area);
+        }
+
+        public static void FindTriangleArea(int x, int y, int z)
+        {
+            
+            double s = (x + y + z) / 2;
+            double area = Math.Sqrt(s * (s - x) * (s - y) * (s - z));
+
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ mindbox/mindbox/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace mindbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius = 5;
+
+            findarea.area.findCircleArea(radius);
+
+            int x = 1, y = 1, z = 1;
+            findarea.area.findTriangleArea(x, y, z);
+        }
+    }
+}
